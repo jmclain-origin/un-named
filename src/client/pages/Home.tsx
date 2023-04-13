c98f1e3148e660fc530cef7f48f095460f97ddf1
Josh McLain <joshmclain45@gmail.com>
2023-04-12 22:13:49 -0700

    📦 NEW: footer

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -1,12 +1,14 @@
 import Hero from "../components/Home/Hero";
 import Features from "../components/Home/Features";
 import Testimonials from "../components/Home/Testimonials";
+import Footer from "../components/Home/Footer";
 export default function Home() {
   return (
     <>
       <Hero />
       <Features />
       <Testimonials />
+      <Footer />
     </>
   );
 }
